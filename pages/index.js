bde452c4b0fcf5a6383b2e170299da9c5735b9e5
irfan fauzi <irfanwintech@yahoo.com>
2022-08-23 23:22:56 +0700

    tailwind config

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,7 @@ export default function Home() {
         <meta name="description" content="ilmu pemhetahuan" />
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi ea id odio perferendis sed, assumenda placeat quis, magni repudiandae tenetur impedit accusantium fuga optio deleniti laboriosam voluptas commodi mollitia cum!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem nisi accusamus quod illum optio consequuntur, tempora necessitatibus tenetur ab itaque voluptatum porro sit sint, dolores praesentium atque? Odio, corrupti asperiores.</p>
+      <p className="navbar">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci, culpa.</p>
     </div>
   )
 }
