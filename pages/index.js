999c6a6f6e95a6ee51c49e20d1bc6edc0ee36b46
irfan fauzi <irfanwintech@yahoo.com>
2022-08-23 22:27:21 +0700

    init

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Medium, where good idea find you</title>
+        <meta name="description" content="ilmu pemhetahuan" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi ea id odio perferendis sed, assumenda placeat quis, magni repudiandae tenetur impedit accusantium fuga optio deleniti laboriosam voluptas commodi mollitia cum!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem nisi accusamus quod illum optio consequuntur, tempora necessitatibus tenetur ab itaque voluptatum porro sit sint, dolores praesentium atque? Odio, corrupti asperiores.</p>
+    </div>
+  )
+}
